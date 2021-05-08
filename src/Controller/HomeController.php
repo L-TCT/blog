3455e24a97b59961bdfd02a516bbdd2f31b2dfe4
Laurence TURCOTTI <69862469+L-TCT@users.noreply.github.com>
2021-05-08 16:53:14 +0200

    liste + vue article fonctionne

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +15,22 @@ class HomeController extends AbstractController
      */
     public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'articles' => $articleRepository->allArticles(),
+            'articles' => $articles,
         ]);
     }
+    
+    /**
+     * @Route("/view/{id}", name="view")
+     */
+    public function show(int $id, ArticleRepository $articleRepository): Response
+    {
+        $article = $articleRepository->find($id);
+        return $this->render('view/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+
 }
